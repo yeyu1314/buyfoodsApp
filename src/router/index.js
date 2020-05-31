213d5a88e1ccd80119f71ce8a3af5fbd7ca9a936
yeyu1314 <2454425375@qq.com>
2020-05-31 19:17:06 +0800

    food finsh

--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,69 @@
 /*
 路由器对象模块
-*/
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import MSite from '../pages/MSite/MSite'
-import Search from '../pages/Search/Search'
-import Order from '../pages/Order/Order'
-import Profile from '../pages/Profile/Profile'
-import Login from '../pages/Login/Login'
-import Shop from '../pages/Shop/Shop.vue'
-import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods'
-import ShopRatings from '../pages/Shop/ShopGoods/ShopRatings'
-import ShopInfo from '../pages/Shop/ShopGoods/ShopInfo'
 
-Vue.use(VueRouter)//声明使用插件
+
+// import MSite from '../pages/MSite/MSite.vue'
+// import Search from '../pages/Search/Search.vue'
+// import Order from '../pages/Order/Order.vue'
+// import Profile from '../pages/Profile/Profile.vue'
+
+const MSite = () => import('../pages/MSite/MSite.vue')
+const Search = () => import('../pages/Search/Search.vue')
+const Order = () => import('../pages/Order/Order.vue')
+const Profile = () => import('../pages/Profile/Profile.vue')
+
+import Login from '../pages/Login/Login.vue'
+import Shop from '../pages/Shop/Shop.vue'
+import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
+import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue'
+import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
+
+// 声明使用插件
+Vue.use(VueRouter)
 
 export default new VueRouter({
-  //所有路由
-  routes : [
+  // 所有路由
+  routes: [
     {
-      path : '/msite',
-      component :MSite,
-      meta : {
-        showFooter : true
+      path: '/msite',
+      component: MSite, // 返回路由组件的函数, 只有执行此函数才会加载路由组件, 这个函数在请求对应的路由路径时才会执行
+      meta: {
+        showFooter: true
       }
     },
     {
-      path : '/search',
-      component :Search,
-      meta : {
-        showFooter : true
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
       }
     },
     {
-      path : '/order',
-      component :Order,
-      meta : {
-        showFooter : true
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
       }
     },
     {
-      path : '/profile',
-      component :Profile,
-      meta : {
-        showFooter : true
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
       }
     },
-    {//默认显示
-      path : '/',
-      redirect : '/msite'
+    {
+      path: '/',
+      redirect: '/msite'
     },
-    {//登录
-      path : '/login',
-      component :Login
+    {
+      path: '/login',
+      component: Login
     },
-    {// 商品
+    {
       path: '/shop',
       component: Shop,
       children: [
@@ -73,9 +82,8 @@ export default new VueRouter({
         {
           path: '',
           redirect: '/shop/goods'
-        }
+        },
       ]
-    }
+    },
   ]
-  
 })
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,9 @@
 /*
 通过mutation间接更新state的多个方法的对象
 */
-import {RECEIVE_ADDRESS,RECEIVE_CATEGORYS,RECEIVE_SHOPS,RECEIVE_USER_INFO,RESET_USER_INFO,RECEIVE_INFO} from './mutation_types'
-import {reqAddress,reqFoodCategorys,reqShops,reqUserInfo,reqLogout, reqShopInfo} from '../api'
+import {RECEIVE_ADDRESS,RECEIVE_CATEGORYS,RECEIVE_SHOPS,RECEIVE_USER_INFO,RESET_USER_INFO,RECEIVE_INFO, RECEIVE_RATINGS,RECEIVE_GOODS,
+  INCREMENT_FOOD_COUNT,DECREMENT_FOOD_COUNT,RECEIVE_SEARCH_SHOPS,CLEAR_CART} from './mutation_types'
+import {reqAddress,reqFoodCategorys,reqShops,reqUserInfo,reqLogout, reqShopInfo,reqShopRatings,reqShopGoods,reqSearchShop} from '../api'
 export default {
   //异步获取地址
   async getAddress({commit, state}) {
@@ -58,10 +59,56 @@ export default {
   // 异步获取商家信息
   async getShopInfo({commit}) {
     const result = await reqShopInfo()
-    console.log(result)
     if (result.code === 0) {
       const info = result.data
       commit(RECEIVE_INFO, {info})
+    }
+  },
+
+  // 异步获取商家评价列表
+  async getShopRatings({commit}, callback) {
+    const result = await reqShopRatings()
+    if (result.code === 0) {
+      const ratings = result.data
+      commit(RECEIVE_RATINGS, {ratings})
+      // 数据更新了, 通知一下组件
+      callback && callback()
+    }
+  },
+
+  // 异步获取商家商品列表
+  async getShopGoods({commit}, callback) {
+    const result = await reqShopGoods()
+    if (result.code === 0) {
+      const goods = result.data
+      commit(RECEIVE_GOODS, {goods})
+      // 数据更新了, 通知一下组件
+      callback && callback()
+    }
+  },
+
+  // 同步更新food中的count值
+  updateFoodCount({commit}, {isAdd, food}) {
+    if (isAdd) {
+      commit(INCREMENT_FOOD_COUNT, {food})
+    } else {
+      commit(DECREMENT_FOOD_COUNT, {food})
+    }
+  },
+
+  // 同步清空购物车
+  clearCart({commit}) {
+    commit(CLEAR_CART)
+  },
+
+  // 异步获取商家商品列表
+  async searchShops({commit, state}, keyword) {
+
+    const geohash = state.latitude + ',' + state.longitude
+    const result = await reqSearchShop(geohash, keyword)
+    if (result.code === 0) {
+      const searchShops = result.data
+      commit(RECEIVE_SEARCH_SHOPS, {searchShops})
     }
   },
 }
